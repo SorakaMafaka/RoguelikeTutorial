85db9d27664221a3189142e579bf467f8b3abff5
Matt <chomikmaster10@wp.pl>
2020-07-03 12:26:45 +0100

    Setting up

---
+++ main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import tcod
+
+
+def main() -> None:
+    screen_width = 80
+    screen_height = 50
+
+
+print("Hello World!")
+
+# This line explained in detail:
+# https://stackoverflow.com/questions/419163/what-does-if-name-main-do/419185#419185
+if __name__ == "__main__":
+    main()
